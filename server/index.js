275e514c5b688db0b74a9278378791159e412a22
tuongnc <s3818196@rmit.edu.vn>
2021-06-28 20:42:57 +0700

    add connection

--- server/index.js
+++ server/index.js
@@ -3,4 +3,18 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const express = require('express');
+const app = express();
+
+app.use(bodyParser.json({limit : "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit : "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://tuong:tuong@cluster0.nm2wa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() =>app.listen(PORT,()=> console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) =>console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
