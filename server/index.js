3c3e4759ea1b37f238c89fa6af0ca58aa1c10243
tuongnc <s3818196@rmit.edu.vn>
2021-06-28 21:28:29 +0700

    Router

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+
+import postRouter from './routes/post.route.js'
+
 const app = express();
+
+app.use('/posts', postRouter);
 
 app.use(bodyParser.json({limit : "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit : "30mb", extended: true}));
