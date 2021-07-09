daff98034bb6664a6f344e2b22a19882b8e4c023
tuongnc <s3818196@rmit.edu.vn>
2021-07-09 22:37:58 +0700

    layout FORM

--- server/index.js
+++ server/index.js
@@ -8,18 +8,18 @@ import postRouter from './routes/post.route.js'
 
 const app = express();
 
-app.use('/posts', postRouter);
 
-app.use(bodyParser.json({limit : "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit : "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRouter);
 const CONNECTION_URL = 'mongodb+srv://tuong:tuong@cluster0.nm2wa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() =>app.listen(PORT,()=> console.log(`Server is running on port: ${PORT}`)))
-    .catch((error) =>console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
 
